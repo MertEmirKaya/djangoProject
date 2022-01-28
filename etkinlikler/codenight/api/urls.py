7c981330f40455bf17d7e6e4a172ed85a8122964
Mert Kaya <mertemir_54@outlook.com>
2022-01-28 20:06:42 +0300

    codenight detay sayfasını oluşturdum

--- etkinlikler/codenight/api/urls.py
+++ etkinlikler/codenight/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns=[
     path('codenights/',views.CreateListCodenightAPIView.as_view(),name='Codenight-listesi'),
+    path('codenights/<int:pk>',views.CodeNightDetailAPIView.as_view(),name='Codenight-detay')
 ]
--- etkinlikler/codenight/api/views.py
+++ etkinlikler/codenight/api/views.py
@@ -18,3 +18,32 @@ class CreateListCodenightAPIView(APIView):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+class CodeNightDetailAPIView(APIView):
+    def get_object(self,pk):
+        codenight_instance=get_object_or_404(CodeNight,pk=pk)
+        return codenight_instance
+
+
+    def get(self,request,pk):
+        codenight=self.get_object(pk=pk)    
+        serializer=CodeNightSerializer(codenight)
+        return Response(serializer.data )
+    
+    def put(self,request,pk):
+        codenight=self.get_object(pk=pk) 
+        serializer=CodeNightSerializer(codenight,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self,request,pk):
+        codenight=self.get_object(pk=pk) 
+        codenight.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
